52a2f84ae2814ba5e3d41c344cd0b9225ce66f84
João Vitor Barbosa <j.vs.b13@gmail.com>
2023-06-22 21:24:17 -0300

    Atualiza src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,14 @@
 // SPDX-License-Identifier:  MIT
 // Copyright (C) 2023 - Prof. Igor Machado Coelho
+// ?(jota-ve): Devo atualizar o copyright acima para os integrantes do grupo
+
 #include <memory>
 //
 #include <fmt/core.h>  // NOLINT
-//
-#include <revisao-c-cpp/aluno.hpp>
-#include <revisao-c-cpp/exemplo.hpp>
 
 using fmt::print;
 
-int main() {
-  print("exercicio1(\"1 3\")={}\n", exercicio1("1 3"));
-  print("exemplo_cpp_scan(\"3 1 2 3\")={}\n", exemplo_cpp_scan("3 1 2 3"));
-
-  return 0;
-}
+int main() { return 0; }
 
 // Ctrl+Shift+P
 // CMake: build
