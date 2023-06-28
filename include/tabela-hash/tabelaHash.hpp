0d154d39bd1e76767c7a0f65b00cff2e1c7e49e0
João Vitor Barbosa <j.vs.b13@gmail.com>
2023-06-27 22:44:35 -0300

    Adiciona prints para debug

--- include/tabela-hash/tabelaHash.hpp
+++ include/tabela-hash/tabelaHash.hpp
@@ -20,34 +20,51 @@
 using fmt::print;  // NOLINT
 using scn::scan;   // NOLINT
 
-constexpr int TAM{10};
+constexpr int TAM{3};
 
 class TabelaHash {
  public:
   bool insere(std::string chave, int valor) {
-    if (this->tamanho() == TAM) return false;
+    print("Inserindo chave \"{}\"\n", chave);
+    if (this->tamanho() == TAM) {
+      print("Tabela Cheia!\n");
+      return false;
+    }
 
     int idx = this->hash(chave);
     while (std::get<0>(this->items.at(idx)) != "" &&
-           std::get<0>(this->items.at(idx)) != chave)
+           std::get<0>(this->items.at(idx)) != chave) {
+      print("idx {} já contém chave \"{}\", indo para o próximo idx...\n", idx,
+            std::get<0>(this->items.at(idx)));
       idx++;
+    }
 
+    print("Guardando \"{}\" no idx {}\n", chave, idx);
     this->items.at(idx) = std::make_tuple(chave, valor);
     this->N++;
+    print("\n");
     return true;
   }
 
   int recupera(std::string chave) {
+    print("Recuperando chave \"{}\"\n", chave);
     int idx = this->hash(chave);
     int cont{0};
     while (std::get<0>(this->items.at(idx)) != chave &&
            cont < this->tamanho()) {
+      print("idx {} contém chave \"{}\", indo para o próximo idx...\n", idx,
+            std::get<0>(this->items.at(idx)));
       idx++;
       cont++;
     }
 
-    if (std::get<0>(this->items.at(idx)) == chave)
+    if (std::get<0>(this->items.at(idx)) == chave) {
+      print("Encontrou chave \"{}\" no idx {}\n\n", chave, idx);
       return std::get<1>(this->items.at(idx));
+    }
+
+    print("Chave \"{}\" não está na tabela\n", chave);
+    print("\n");
 
     return 0;
   }
@@ -58,5 +75,10 @@ class TabelaHash {
   std::array<std::tuple<std::string, int>, TAM> items;
   int N{0};
 
-  int hash(std::string chave) { return static_cast<int>(chave.length()) % TAM; }
+  int hash(std::string chave) {
+    auto idx = static_cast<int>(chave.length()) % TAM;
+    print("hash(\"{}\") -> {}\n", chave, idx);
+    print("\n");
+    return idx;
+  }
 };
